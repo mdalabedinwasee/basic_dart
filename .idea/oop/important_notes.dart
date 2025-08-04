58f6a5aaf3ecdede3cef504b650b2460994e2842
Md. Al Abedin Wasee <mdalabedinwasee@gmail.com>
2025-08-04 17:37:00 +0600

    employee added

--- .idea/oop/important_notes.dart
+++ .idea/oop/important_notes.dart
@@ -1,4 +1,4 @@
 /* Serial list
   1.class_object, 2.person, 3.human, 4.inheritance, 5.implementation, 6.polymorphism, 7.computer, 8.computer_shop, 9.hp_laptop,
-  10.apple_laptop
+  10.apple_laptop, 11.employee, 
    */
